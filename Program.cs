ca266e8a6c782d4dd2a3f04bad46d5336be10f8f
JESUS NAVARRO <JUANJESUS_NAVARRO@Jabil.com>
2023-05-03 17:10:15 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ PRODUCER!
+ */
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQClient;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "", UserName = "writer-stg", Password= "" };//IP Address from RabbitMQ server
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+string json = JsonConvert.SerializeObject(new Serial());
+var body = Encoding.UTF8.GetBytes(json);
+
+channel.BasicPublish(exchange: "jemsmm.exchange",
+    routingKey:"",
+    basicProperties: null,
+    body: body);
+Console.WriteLine($" [x] Sent {json}");
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
